886d4b95c692f2071674420d38e2e343e895a194
Panda (Thomas) Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2018-09-30 19:34:47 +0100

    Add additional gulp modes

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,25 @@ gulp.task('css', () => {
          .pipe(gulp.dest('_site/style/'));
 });
 
+gulp.task('images', () => {
+  return gulp.src('src/images/**/*.png')
+          .pipe(gulp.dest('_site/images/'));
+})
+
 gulp.task('generate', shell.task('eleventy'));
 
-gulp.task('assets', gulp.parallel('css'));
+gulp.task('generate-dev', shell.task('eleventy', { ignoreErrors: true }));
+
+gulp.task('assets', gulp.parallel('css', 'images'));
 
 gulp.task('build', gulp.series('generate', 'assets'));
 
 gulp.task('watch', () => {
-  gulp.watch('src/**/*.{md,html,njk}', gulp.series('generate'));
+  gulp.watch(
+    ['.eleventy.js', 'src/**/*.{md,html,njk}'],
+    gulp.series('generate-dev')
+  );
   gulp.watch('src/style/**/*.less', gulp.series('css'));
+  gulp.watch('src/images/**/*.png', gulp.series('images'));
 });
 
